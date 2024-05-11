4588ae34c37c4381f7269b57a6363edb0514cdb3
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2024-05-11 18:24:02 -0300

    Visualização de senhas da páginas: cadastro e login

--- js/script.js
+++ js/script.js
@@ -5,4 +5,28 @@ let listaUsuarios = [
     {nomeUsuario:"9027908",senhaUsuario:"017972b8d104efcf41a8f034d3"},
 ];
 
+const alteraVisao = (inputSenha)=>{
+    if(inputSenha.type == "password"){
+        inputSenha.setAttribute("type","text");
+    }else{
+        inputSenha.setAttribute("type","password");
+    }
+}
 
+
+let iconEye = document.querySelector("i");
+iconEye.addEventListener('click', function(){
+    
+    let inputSenha = document.querySelector("#idSenha");
+
+    if(this.className == "fa-regular fa-eye-slash" ){
+        this.setAttribute("class","fa-regular fa-eye");
+        alteraVisao(inputSenha);
+    }else{
+        this.setAttribute("class","fa-regular fa-eye-slash");
+        alteraVisao(inputSenha);
+    }
+    console.log(iconEye);
+});
+
+console.log("pinto");
