df29e7b480dceed14ef4c7d954fa14adeadb7add
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2024-05-15 16:25:17 -0300

    LOGIN 100%

--- js/auth.js
+++ js/auth.js
@@ -1,11 +1,40 @@
-// Verifica se há um usuário logado no localStorage
-const usuarioLogado = JSON.parse(localStorage.getItem("usuario-logado"));
-if (usuarioLogado) {
-    // Exibe uma mensagem junto com o nome do usuário
-    const mensagem = "Olá, ";
-    const userNameElement = document.getElementById("user-name");
-    userNameElement.textContent = mensagem + usuarioLogado.nomeUsuario;
-    // Exibe o elemento onde o nome do usuário está contido
-    const userInfoElement = document.getElementById("user-info");
-    userInfoElement.style.display = "block";
+
+
+// Função para verificar se o usuário está logado
+function verificarUsuarioLogado() {
+    // Verifica se há um usuário logado no localStorage
+    const usuarioLogado = JSON.parse(localStorage.getItem("usuario-logado"));
+    if (usuarioLogado) {
+        // Exibe uma mensagem junto com o nome do usuário
+        const mensagem = "Olá, ";
+        const userNameElement = document.getElementById("user-name");
+        userNameElement.textContent = mensagem + usuarioLogado.nomeUsuario;
+        
+        const userInfoElement = document.getElementById("user-info");
+        userInfoElement.style.display = "block";
+
+        // Altera o link de login para logout
+        const loginLink = document.querySelector(".links-direita .login-link");
+        loginLink.innerHTML = "<a href='#' onclick='deslogarUsuario()'>Logout</a>";
+    } else {
+       
+        const userInfoElement = document.getElementById("user-info");
+        userInfoElement.style.display = "none";
+
+        // Restaura o link de login
+        const loginLink = document.querySelector(".links-direita .login-link");
+        loginLink.innerHTML = "<a href='./paginas/login.html'>Login</a>";
+    }
 }
+
+// Função para deslogar o usuário
+function deslogarUsuario() {
+    
+    localStorage.removeItem("usuario-logado");
+    sessionStorage.removeItem("token");
+    
+    verificarUsuarioLogado();
+}
+
+
+document.addEventListener("DOMContentLoaded", verificarUsuarioLogado);
