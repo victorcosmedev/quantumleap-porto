3b4f8890d8e00de2072bee3f4ba3e852ff38abd3
victorcosmedev <victorcosme.profissional@gmail.com>
2024-08-25 19:33:51 -0300

    Ajustada a página Home para o react.

--- quantumleap-porto/src/routes/Home/index.tsx
+++ quantumleap-porto/src/routes/Home/index.tsx
@@ -1,43 +1,48 @@
-export default function Home(){
-    return(
+import style from "./Home.module.css";
+import motoristaAzul from "../../img/motorista-azul.jpeg";
+import caminhaoPorto from "../../img/caminhao-porto.jpeg";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+    return (
         <main>
-        <div className="grid-main">
-          <div className="box-banner-cadastro">
-            <div>
-              <h1>Problemas com seu carro?</h1>
-              <h2>Nós temos a solução</h2>
-              <a className="botao-cadastro" href="./paginas/cadastro.html">Cadastre-se já</a>
+            <div className={style.gridMain}>
+                <div className={style.boxBannerCadastro}>
+                    <div>
+                        <h1>Problemas com seu carro?</h1>
+                        <h2>Nós temos a solução</h2>
+                        <Link to="/cadastro" className={style.botaoCadastro}>Cadastre-se já</Link>
+                    </div>
+                </div>
+                <div className={style.boxImgMotorista}>
+                    <img src={motoristaAzul} alt="" />
+                </div>
+                <div className={style.boxListaProblemas}>
+                    <div>
+                        <h2>Problemas como:</h2>
+                        <nav className={style.listaProblemas}>
+                            <ul>
+                                <li>Alinhamento</li>
+                                <li>Ar-condicionado</li>
+                                <li>Arrefecimento</li>
+                                <li>Balanceamento e Geometria</li>
+                                <li>Correias</li>
+                                <li>Discos e Pastilhas de Freio</li>
+                                <li>Embreagens</li>
+                                <li>Filtros</li>
+                                <li>Velas de Ignição</li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div className={style.boxImgCaminhao}>
+                    <img src={caminhaoPorto} alt="" />
+                </div>
+                <div className={style.boxIniciarChat}>
+                    <h1>Precisa de ajuda? Fale conosco!</h1>
+                    <a className={style.botaoIniciarChat} href="./paginas/chat.html">Iniciar Chat</a>
+                </div>
             </div>
-          </div>
-          <div className="box-img-motorista">
-            <img src="./img/motorista-azul.jpeg" alt="" />
-          </div>
-          <div className="box-lista-problemas">
-            <div>
-              <h2>Problemas como:</h2>
-              <nav className="lista-problemas">
-                <ul>
-                  <li>Alinhamento</li>
-                  <li>Ar-condicionado</li>
-                  <li>Arrefecimento</li>
-                  <li>Balanceamento e Geometria</li>
-                  <li>Correias</li>
-                  <li>Discos e Pastilhas de Freio</li>
-                  <li>Embreagens</li>
-                  <li>Filtros</li>
-                  <li>Velas de Ignição</li>
-                </ul>
-              </nav>
-            </div>
-          </div>
-          <div className="box-img-caminhao">
-            <img src="./img/caminhao-porto.jpeg" alt="" />
-          </div>
-          <div className="box-iniciar-chat">
-            <h1>Precisa de ajuda? Fale conosco!</h1>
-            <a className="botao-iniciar-chat" href="./paginas/chat.html">Iniciar Chat</a>
-          </div>
-        </div>
-      </main>
+        </main>
     );
 }
