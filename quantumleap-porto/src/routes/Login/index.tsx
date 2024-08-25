dcdda40115b2bfdee9113b277eda87c300d38792
victorcosmedev <victorcosme.profissional@gmail.com>
2024-08-25 17:28:56 -0300

    Formatada a página de login com CSS Modules

--- quantumleap-porto/src/routes/Login/index.tsx
+++ quantumleap-porto/src/routes/Login/index.tsx
@@ -1,33 +1,34 @@
-import styles from "./Login.module.css";
+import style from "./Login.module.css";
 
 export default function Login(){
     return(
-        <main className={styles.containerForm}>
-        <form className={styles.formulario}>
-            <fieldset className={styles.quadrado}>
-            <h1>Acessar Conta</h1>
-            <p>Preencha seus dados de acesso para continuar.</p>
-            <div className="div-campo">
-                <label htmlFor="cpf">Email</label>
-                <input
-                id="cpf"
-                className="formatacao-entrada-campo"
-                type="password"
-                name="senha"
-                placeholder="CPF / CNPJ"
-                />
-            </div>
-            <div className="div-campo">
-                <label htmlFor="senha">Senha</label>
-                <input
-                id="senha"
-                className="formatacao-entrada-campo"
-                type="password"
-                name="senha"
-                placeholder="Senha"
-                />
-            </div>
-            <button className="enviar" type="submit">Enviar</button>
+        <main className={style.containerForm}>
+        <form className={style.formulario}>
+            <fieldset className={style.quadrado}>
+                <h1>Acessar Conta</h1>
+                <p>Preencha seus dados de acesso para continuar.</p>
+                
+                <div className={style.divCampo}>
+                    <label htmlFor="cpf">Email</label>
+                    <input
+                    id="cpf"
+                    className={style.formatacaoEntradaCampo}
+                    type="password"
+                    name="senha"
+                    placeholder="CPF / CNPJ"
+                    />
+                </div>
+                <div className={style.divCampo}>
+                    <label htmlFor="senha">Senha</label>
+                    <input
+                    id="senha"
+                    className={style.formatacaoEntradaCampo}
+                    type="password"
+                    name="senha"
+                    placeholder="Senha"
+                    />
+                </div>
+                <button className={style.enviar} type="submit">Enviar</button>
             </fieldset>
         </form>
         </main>
