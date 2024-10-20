c2184f8e357b98c57906021b9994ed93b07573a8
victorcosmedev <victorcosme.profissional@gmail.com>
2024-10-19 23:31:16 -0300

    Página de login formatada.

--- implementacao-next/src/app/login/page.tsx
+++ implementacao-next/src/app/login/page.tsx
@@ -80,18 +80,18 @@ export default function Login(){
     
   return(
     <main className="mx-auto lg:py-[20vh] lg:px-[25vw]">
-      <form className="border-2 border-slate-400 bg-slate-100 flex justify-center lg:rounded-2xl lg:py-8 lg:px-9" onSubmit={envioFormulario}>
-          <fieldset className="w-full flex flex-col gap-3 lg:text-lg lg:font-medium">
+      <form className="border lg:border-slate-400 bg-slate-100 flex justify-center lg:rounded-2xl lg:py-8 lg:px-9 lg:h-fit sm:py-8 sm:px-[10vw] sm:h-[80vh] items-center" onSubmit={envioFormulario}>
+          <fieldset className="w-full flex flex-col lg:gap-3 lg:text-lg lg:font-medium sm:gap-5 sm:text-lg sm:font-medium">
               <div className="text-center">
-                <h1 className="lg:font-semibold lg:text-xl">Acessar Conta</h1>
-                <p className="lg:text-base">Não tem cadastro? <Link href="/cadastro">Cadastre-se já!</Link></p>
+                <h1 className="lg:font-semibold lg:text-xl sm:font-semibold sm:text-xl">Acessar Conta</h1>
+                <p className="lg:text-base sm:text-base">Não tem cadastro? <Link href="/cadastro">Cadastre-se já!</Link></p>
               </div>
               
-              <div className="flex flex-col w-full lg:gap-1">
+              <div className="flex flex-col w-full lg:gap-1 sm:gap-1">
                   <label htmlFor="cpf">CPF</label>
                   <input
                   id="cpf"
-                  className="border lg:py-1 lg:px-3 lg:text-sm rounded-md"
+                  className="border rounded-md lg:py-1 lg:px-3 lg:text-sm sm:py-1 sm:px-3 sm:text-sm"
                   type="text"
                   name="cpf"
                   placeholder="CPF / CNPJ"
@@ -99,18 +99,18 @@ export default function Login(){
                   required
                   />
               </div>
-              <div className="flex flex-col w-full lg:gap-1 lg:text-lg">
+              <div className="flex flex-col w-full lg:gap-1 sm:gap-1">
                   <label htmlFor="senha">Senha</label>
                   <input
                   id="senha"
-                  className="border lg:py-1 lg:px-3 lg:text-sm rounded-md"
+                  className="border rounded-md lg:py-1 lg:px-3 lg:text-sm sm:py-1 sm:px-3 sm:text-sm"
                   type="password"
                   name="senha"
                   placeholder="Senha"
                   required
                   />
               </div>
-              <button className="justify-self-start w-fit text-white bg-azul-escuro lg:text-sm lg:py-1 lg:px-4 lg:rounded-md" type="submit">Enviar</button>
+              <button className="justify-self-start w-fit text-white bg-azul-escuro lg:text-sm lg:py-1 lg:px-4 lg:rounded-md sm:text-sm sm:py-1 sm:px-4 sm:rounded-md" type="submit">Enviar</button>
               <p className={`${classeMensagem === "sucesso" ? "text-green-600" : "text-red-700"}`}>{msgStatusEnvio}</p>
           </fieldset>
       </form>
