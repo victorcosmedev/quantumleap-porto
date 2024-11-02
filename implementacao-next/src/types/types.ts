d294bcdd48c0f0e107bf8f83327d9f73d4d569ce
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2024-11-02 17:46:28 -0300

    Criação de Login + Consumo de API back

---
+++ implementacao-next/src/types/types.ts
@@ -0,0 +1,10 @@
+export type TipoVeiculo = {
+    idVeiculo: number; 
+    montadoraVeiculo: string; 
+    modeloVeiculo: string; 
+    anoVeiculo: number; 
+    quantidadeQuilometros: number; 
+    placaVeiculo: string; 
+    nomeCliente: string | null; 
+    idCliente: number | null; 
+};
