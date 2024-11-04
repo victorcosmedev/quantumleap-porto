3a61c9f025230b60d1ee2208b93d6ac266f13cfb
arthur-pulini <arthurpulini@gmail.com>
2024-11-03 22:20:22 -0300

    Implementado os métodos de PUT e DELETE do usuário.

--- implementacao-next/src/app/area-cliente/page.tsx
+++ implementacao-next/src/app/area-cliente/page.tsx
@@ -1,25 +1,32 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useSession } from "next-auth/react";
-import { TipoVeiculo } from "@/types/types";
+import { signOut, useSession } from "next-auth/react";
+import { TipoCliente, TipoVeiculo } from "@/types/types";
 import { useRouter } from "next/navigation";
+import { render } from "react-dom";
+import Link from "next/link";
 
 export default function AreaCliente() {
   const { data: session } = useSession();
-  const [classeBotaoAreaDoCliente, setClasseBotaoAreaDoCliente] = useState("text-white bg-azul-escuro");
+  const [classeBotaoAreaDoCliente, setClasseBotaoAreaDoCliente] = useState(
+    "text-white bg-azul-escuro"
+  );
   const [listaDeVeiculos, setListaDeVeiculos] = useState<TipoVeiculo[]>([]);
+  const [cliente, setCliente] = useState<TipoCliente>({
+    idCliente: 0,
+    nomeCliente: "",
+    emailCliente: "",
+    telefoneCliente: "",
+    senhaCliente: "",
+    clientePorto: false,
+    localizacaoCliente: "",
+  });
   const router = useRouter();
-
-  const botaoCadastroVeiculoOnClick = () => {
-    setClasseBotaoCadastroDoVeiculo(styles.buttonSelected);
-    setClasseBotaoAreaDoCliente("");
-    setMostrarAreaDoCliente(false);
-  };
 
   useEffect(() => {
     if (session) {
-      const chamadaApi = async () => {
+      const buscaVeiculos = async () => {
         try {
           const response = await fetch(
             `http://localhost:8080/veiculos/buscarVeiculoCliente/${session.idCliente}`,
@@ -38,115 +45,107 @@ export default function AreaCliente() {
           setListaDeVeiculos(data);
 
           setTimeout(() => {
-
             console.log(listaDeVeiculos);
           }, 2000);
         } catch (error) {
           console.error("Erro ao buscar veículos do cliente:", error);
         }
       };
-    
-      chamadaApi();
+
+      buscaVeiculos();
+      const buscaUsuario = async (idCliente: number) => {
+        console.log(`http://localhost:8080/clientes/${idCliente}`);
+        try {
+          const response = await fetch(
+            `http://localhost:8080/clientes/${idCliente}`,
+            {
+              method: "GET",
+            }
+          );
+          const dadosUsuario = await response.json();
+          setCliente(dadosUsuario);
+        } catch (error) {
+          console.log("Não foi possível carregar os dados do usuário.", error);
+        }
+      };
+
+      buscaUsuario(session.idCliente);
     } else {
       router.push("/");
     }
   }, [session, router]);
 
-  const [veiculo, setVeiculo] = useState<TipoVeiculo>({
-    idVeiculo: 0,
-    montadoraVeiculo: "",
-    modeloVeiculo: "",
-    anoVeiculo: 0.0,
-    quantidadeQuilometros: 0.0,
-    placaVeiculo: "",
-    nomeCliente: session?.nomeCliente ?? "",
-    idCliente: session?.idCliente ?? 0,
-  });
+  const [exibirDadosUsuario, setExibirDadosUsuario] = useState(true);
 
-  const handleSubmit = async (e:React.FormEvent<HTMLFormElement>)=>{
-    e.preventDefault();
-    console.log("submit")
-    try{
-      const response = await fetch('http://localhost:8080/veiculos', {
-        method: 'POST',
-        headers:{
-          "Content-Type" : "application/json"
-        },
-        body: JSON.stringify(veiculo)
-      });
+  useEffect(() => {
+    renderizaAreaUsuario();
+  }, [exibirDadosUsuario])  
 
-      if(response.ok){
-        alert("Veículo cadastrado com sucesso!");
-        setVeiculo({
-          idVeiculo: 0,
-          montadoraVeiculo: "",
-          modeloVeiculo: "",
-          anoVeiculo: 0.0,
-          quantidadeQuilometros: 0.0,
-          placaVeiculo: "",
-          nomeCliente: session?.nomeCliente ?? "",
-          idCliente: session?.idCliente ?? 0,
-        });
-        window.location.reload();
-      }
+  const deletarUsuario = () => {
+    // console.log(`http://localhost:8080/clientes/deletarCliente/${cliente.idCliente}`);
+    try {
+      const response = fetch(`http://localhost:8080/clientes/deletarCliente/${cliente.idCliente}`, {
+        method: "DELETE",
+      })
+
+      logout();
+
     } catch(error){
-      console.error("Erro ao cadastrar veículo:", error);
+      console.log("Não foi possível deletar o usuário.", error);
     }
   }
 
-  const buscarVeiculoParaEditar = (idVeiculo: number) => {
-    const veiculoSelecionado: TipoVeiculo = listaDeVeiculos.find((veiculo) => veiculo.idVeiculo == idVeiculo);
-    console.log(veiculoSelecionado);
-    setVeiculo(veiculoSelecionado);
+  async function logout() {
+    await signOut({
+      redirect: false,
+    });
+    router.replace("/");
   }
 
-  const handleDelete = async (idVeiculo: number) => {
-    try {
-      const response = await fetch(`http://localhost:8080/veiculos/deletarVeiculo/${idVeiculo}`, {
-        method: "DELETE"
-      });
-
-      if(response.ok){
-        alert("Veículo deletado com sucesso!");
-        window.location.reload();
-        
-      }
-    } catch (error){
-      console.log("Não foi possível cadastrar o veículo.", error);
-    }
-  }
-
-  const handleUpdate = async () => {
-    console.log(veiculo);
-    try {
-      const response = await fetch(`http://localhost:8080/veiculos/atualizarVeiculo/${veiculo.idVeiculo}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(veiculo)
-      });
-    } catch (error) {
-      console.log("Não foi possível atualizar o usuário.", error);
-    }
-  }
-
-  return (
-    <main className="w-full lg:py-[25vh] lg:px-[15vw] lg:text-lg sm:py-[10vh] sm:px-[15vw] sm:text-base">
-      <div className="w-full">
-        <div className="flex lg:gap-3 lg:mb-3 lg:text-xl sm:gap-3 sm:mb-3 sm:text-base">
-          <div className="rounded-md lg:py-1 lg:px-3 font-semibold bg-azul-escuro text-white sm:py-2 sm:px-2">
-            Área do cliente
+  const renderizaAreaUsuario = () => {
+    if (exibirDadosUsuario) {
+      return(
+        <div className="border border-red-600">
+          <ul className="mb-3">
+            <li>
+              <span className="font-semibold">Nome: </span>
+              {cliente.nomeCliente}
+            </li>
+            <li>
+              <span className="font-semibold">Email: </span>{" "}
+              {cliente.emailCliente}
+            </li>
+            <li>
+              <span className="font-semibold">Telefone: </span>
+              {cliente.telefoneCliente}
+            </li>
+            <li>
+              <span className="font-semibold">Cliente Porto: </span>
+              {cliente.clientePorto ? "Sim" : "Não"}
+            </li>
+            <li>
+              <span className="font-semibold">Localização: </span>
+              {cliente.localizacaoCliente}
+            </li>
+          </ul>
+          <div className="flex gap-2">
+            <Link href={`/cadastro/${session?.idCliente}`}
+              className="bg-azul-escuro text-white lg:font-semibold lg:text-base lg:px-3 lg:py-1 lg:rounded-md sm:font-semibold sm:text-xs sm:px-3 sm:py-1 sm:rounded-md me-3"
+            >
+              Editar dados
+            </Link>
+            <Link href={"/"}
+            onClick={() => deletarUsuario()}
+            className="bg-red-600 text-white lg:font-semibold lg:text-base lg:px-3 lg:py-1 lg:rounded-md sm:font-semibold sm:text-xs sm:px-3 sm:py-1 sm:rounded-md me-3"
+          >
+            Excluir usuário
+          </Link>
           </div>
-          <div className="rounded-md font-semibold bg-slate-200 text-slate-300 lg:py-1 lg:px-3 sm:py-2 sm:px-2">
-            Cadastro de veículo
-          </div>
-          <h1>Olá, {session?.nomeCliente}</h1>
         </div>
-        <div className="bg-slate-300 grid lg:grid-cols-2 sm:grid-cols-1 lg:px-[3.5%] lg:py-4 lg:rounded-md lg:gap-3 sm:px-[5%] sm:py-3 sm:rounded-md sm:gap-3">
-
-
-          <form onSubmit={handleSubmit}>
+      )
+    } else {
+      return(
+        <form onSubmit={handleSubmit}>
             <fieldset className="grid grid-cols-2 grid-rows-3 lg:gap-x-2 sm:gap-x-2">
               <div className="w-full lg:py-2 sm:py-2">
                 <label htmlFor="" className="lg:text-base sm:text-sm">
@@ -232,8 +231,117 @@ export default function AreaCliente() {
               Editar
             </button>
           </form>
+      )
+    }
+  }
 
-          <div className="bg-slate-50 lg:px-2 lg:py-2 rounded-md lg:text-sm sm:text-base w-full sm:col-span-2 lg:col-span-1 sm:px-2 sm:py-2 flex flex-col sm:gap-2">
+  const [veiculo, setVeiculo] = useState<TipoVeiculo>({
+    idVeiculo: 0,
+    montadoraVeiculo: "",
+    modeloVeiculo: "",
+    anoVeiculo: 0.0,
+    quantidadeQuilometros: 0.0,
+    placaVeiculo: "",
+    nomeCliente: session?.nomeCliente ?? "",
+    idCliente: session?.idCliente ?? 0,
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:8080/veiculos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(veiculo),
+      });
+
+      if (response.ok) {
+        alert("Veículo cadastrado com sucesso!");
+        setVeiculo({
+          idVeiculo: 0,
+          montadoraVeiculo: "",
+          modeloVeiculo: "",
+          anoVeiculo: 0.0,
+          quantidadeQuilometros: 0.0,
+          placaVeiculo: "",
+          nomeCliente: session?.nomeCliente ?? "",
+          idCliente: session?.idCliente ?? 0,
+        });
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error("Erro ao cadastrar veículo:", error);
+    }
+  };
+
+  const buscarVeiculoParaEditar = (idVeiculo: number) => {
+    const veiculoSelecionado: TipoVeiculo = listaDeVeiculos.find(
+      (veiculo) => veiculo.idVeiculo == idVeiculo
+    );
+    console.log(veiculoSelecionado);
+    setVeiculo(veiculoSelecionado);
+  };
+
+  const handleDelete = async (idVeiculo: number) => {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/veiculos/deletarVeiculo/${idVeiculo}`,
+        {
+          method: "DELETE",
+        }
+      );
+
+      if (response.ok) {
+        alert("Veículo deletado com sucesso!");
+        window.location.reload();
+      }
+    } catch (error) {
+      console.log("Não foi possível cadastrar o veículo.", error);
+    }
+  };
+
+  const handleUpdate = async () => {
+    console.log(veiculo);
+    try {
+      const response = await fetch(
+        `http://localhost:8080/veiculos/atualizarVeiculo/${veiculo.idVeiculo}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(veiculo),
+        }
+      );
+
+      if (response.ok) {
+        alert("Veículo atualizado com sucesso!");
+        window.location.reload();
+      }
+    } catch (error) {
+      console.log("Não foi possível atualizar o usuário.", error);
+    }
+  };
+
+  return (
+    <main className="w-full lg:py-[25vh] lg:px-[15vw] lg:text-lg sm:py-[10vh] sm:px-[15vw] sm:text-base">
+      <div className="w-full">
+        <div className="flex lg:gap-3 lg:mb-3 lg:text-xl sm:gap-3 sm:mb-3 sm:text-base items-center">
+          <button className="rounded-md lg:py-1 lg:px-3 font-semibold bg-azul-escuro text-white sm:py-2 sm:px-2" onClick={() => setExibirDadosUsuario(true)}>
+            Área do cliente
+          </button>
+          <button className="rounded-md lg:py-1 lg:px-3 font-semibold bg-azul-escuro text-white sm:py-2 sm:px-2" onClick={() => setExibirDadosUsuario(false)}>
+            Cadastro de veículo
+          </button>
+          <h1>Olá, {session?.nomeCliente}</h1>
+        </div>
+        <div className="bg-slate-300 grid 2xl:grid-cols-2 sm:grid-cols-1 lg:px-[3.5%] lg:py-4 lg:rounded-md lg:gap-3 sm:px-[5%] sm:py-3 sm:rounded-md sm:gap-3">
+
+          { renderizaAreaUsuario() }
+
+          <div className="bg-slate-50 lg:px-2 lg:py-2 rounded-md lg:text-sm sm:text-base w-full sm:col-span-2 2xl:col-span-1 sm:px-2 sm:py-2 flex flex-col sm:gap-2">
             <h1 className="font-semibold lg:mb-1 lg:text-base sm:text-sm">
               Veículos Cadastrados
             </h1>
@@ -254,14 +362,24 @@ export default function AreaCliente() {
                       {veiculo.montadoraVeiculo}
                     </td>
                     <td className="text-center border">
-                    {veiculo.modeloVeiculo}
+                      {veiculo.modeloVeiculo}
+                    </td>
+                    <td className="text-center border">{veiculo.anoVeiculo}</td>
+                    <td className="text-center border">
+                      {veiculo.placaVeiculo}
                     </td>
                     <td className="text-center border">
-                      {veiculo.anoVeiculo}
-                    </td>
-                    <td className="text-center border">{veiculo.placaVeiculo}</td>
-                    <td className="text-center border">
-                    <button onClick={() => buscarVeiculoParaEditar(veiculo.idVeiculo)}>Editar</button> | <button onClick={() => handleDelete(veiculo.idVeiculo)}>Excluir</button>
+                      <button
+                        onClick={() =>
+                          buscarVeiculoParaEditar(veiculo.idVeiculo)
+                        }
+                      >
+                        Editar
+                      </button>{" "}
+                      |{" "}
+                      <button onClick={() => handleDelete(veiculo.idVeiculo)}>
+                        Excluir
+                      </button>
                     </td>
                   </tr>
                 ))}
